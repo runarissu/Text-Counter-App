24f0bdd80ba9d82ac7cb882bf8503eaff375f079
runarissu <runarissu@gmail.com>
2022-06-03 14:06:36 +0700

    01

---
+++ _next/static/aqtRttYmpRMcu8HXX5eXy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-de8ad027af9aea94.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
